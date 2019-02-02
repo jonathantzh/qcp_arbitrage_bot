34641458a305d3638b412b726f4eb184e1cd74c6
Jonathan Tan <jonathan.x85@gmail.com>
2019-02-02 14:05:08 +0800

    add single instrument query

--- server.js
+++ server.js
@@ -5,12 +5,30 @@ var app = express();
 app.options('*', cors()) // include before other routes
 
 const a = require("./functions/arbitrage");
+const g = require("./functions/general");
 
 app.get('/arbitrageResults', cors(), async (req,res) => {
     console.log("GET on /arbitrageResults received! Getting results...");
     try {
         let arbitrageResults = await a.run();
         res.status(200).send(arbitrageResults);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send();
+    }
+})
+
+app.get('/arbitrageResult', cors(), async (req,res) => {
+    console.log("GET on /arbitrageResult received! Getting result...");
+    try {
+        let instrument = req.query.instrument;
+        if(g.isInstrumentValid(instrument)){
+            let arbitrageResult = await a.runSingle(instrument);
+            res.status(200).send(arbitrageResult);
+        } else {
+            console.log(instrument, "not a valid instrument!");
+            res.status(400).send();
+        }
     } catch (err) {
         console.log(err);
         res.status(500).send();
